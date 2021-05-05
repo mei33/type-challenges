596f1740931bd17bb8ffe6b5b3137c511ce54d2a
Maksim Andreev <mei33.post@gmail.com>
2021-05-05 12:03:01 +0300

    Solve Medium - 15 - Replace All

--- questions/119-medium-replaceall/template.ts
+++ questions/119-medium-replaceall/template.ts
@@ -1,1 +1,13 @@
-type ReplaceAll<S extends string, From extends string, To extends string> = any
+type ReplaceAll<
+    S extends string,
+    From extends string,
+    To extends string
+> = S extends ''
+    ? S
+    : From extends ''
+        ? S
+        : S extends `${From}${infer R1}`
+            ? `${To}${ReplaceAll<R1, From, To>}`
+            : S extends `${infer L2}${infer R2}`
+                ? `${L2}${ReplaceAll<R2, From, To>}`
+                : S
