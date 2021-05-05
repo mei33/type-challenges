51a7dfcd91ea3108113d5b40a720ab426f20675a
Maksim Andreev <mei33.post@gmail.com>
2021-05-05 15:57:22 +0300

    Solve Medium - 23 - Merge

--- questions/599-medium-merge/template.ts
+++ questions/599-medium-merge/template.ts
@@ -1,1 +1,10 @@
-type Merge<F, S> = any;
+type Merge<
+    F extends {[K: string]: unknown},
+    S extends {[P: string]: unknown}
+> = {
+    [K in keyof F | keyof S]: K extends keyof S
+        ? S[K] 
+        : K extends keyof F
+            ? F[K]
+            : never
+};
