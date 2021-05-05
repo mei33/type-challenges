30602f415cfca1671faa66b32c6749f8502ca9d9
Maksim Andreev <mei33.post@gmail.com>
2021-05-05 16:27:07 +0300

    Solve Medium - 24 - CamelCase

--- questions/610-medium-camelcase/template.ts
+++ questions/610-medium-camelcase/template.ts
@@ -1,1 +1,5 @@
-type CamelCase<S> = any;
+type CamelCase<S> = S extends `${infer H}-${infer T}`
+? T extends Capitalize<T>
+    ? `${H}-${CamelCase<T>}`
+    : `${H}${CamelCase<Capitalize<T>>}`
+: S;
