c1c5b8959b04278a3fe2a4f6fc9f4f1218f4a617
Maksim Andreev <mei33.post@gmail.com>
2021-05-05 16:37:15 +0300

    Solve Medium - 24 - KebabCase

--- questions/612-medium-kebabcase/template.ts
+++ questions/612-medium-kebabcase/template.ts
@@ -1,1 +1,5 @@
-type KebabCase<S> = any;
+type KebabCase<S> = S extends `${infer H}${infer T}`
+? T extends Uncapitalize<T>
+    ? `${Uncapitalize<H>}${KebabCase<T>}`
+    : `${Uncapitalize<H>}-${KebabCase<T>}`
+: S;
