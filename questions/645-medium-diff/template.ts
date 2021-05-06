4ec0e8aef89b2205c4caf69f5fc24c22ba79ba39
Maksim Andreev <ma.andreev@corp.vk.com>
2021-05-06 09:55:15 +0300

    Solve Medium - 26 - Diff

--- questions/645-medium-diff/template.ts
+++ questions/645-medium-diff/template.ts
@@ -1,1 +1,5 @@
-type Diff<O, O1> = any
+type Diff<O, O1> = {
+  [K in (keyof O | keyof O1) as Exclude<K, keyof O & keyof O1>]: K extends keyof O
+    ? K extends keyof O1 ? never : O[K]
+    : K extends keyof O1 ? O1[K] : never
+}
